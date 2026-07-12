d26ecbd78354b04002eadb0506f72afc3bcb419b
Kunal Kashyap <kunalkashyap7504@gmail.com>
2026-07-12 19:14:38 +0530

    fixed lightings

--- heart01/src/main.js
+++ heart01/src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader.js";
 
 // ======================
 // Scene
@@ -33,6 +34,10 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.2;
 
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+
 document.body.appendChild(renderer.domElement);
 
 // ======================
@@ -52,6 +57,11 @@ scene.add(ambientLight);
 // Main Light
 const mainLight = new THREE.DirectionalLight(0xffffff, 4);
 mainLight.position.set(5, 5, 5);
+mainLight.castShadow = true;
+
+mainLight.shadow.mapSize.width = 2048;
+mainLight.shadow.mapSize.height = 2048;
+
 scene.add(mainLight);
 
 // Fill Light
@@ -69,6 +79,16 @@ const backLight = new THREE.DirectionalLight(0xffffff, 1);
 backLight.position.set(0, 5, -10);
 scene.add(backLight);
 
+/***********LOADING THE HDR*********/
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load(
+    "/hdr/studio_small_03_1k.hdr",texture => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.environment = texture;
+    });
+
+  
+
 //******************************************************************************************************** */
 //Creating a loader for the GLTF model
 const loader = new GLTFLoader();
@@ -81,10 +101,42 @@ loader.load(
 
         const heart = gltf.scene;
 
+        const box = new THREE.Box3().setFromObject(heart);
+        console.log(box)
+
+
+        const center = box.getCenter(new THREE.Vector3());
+        console.log(center)
+
+
+        heart.position.sub(center); // Center the model
+
+
+        const size = box.getSize(new THREE.Vector3());
+        console.log(size)
+
+        const maxDimension = Math.max(size.x, size.y, size.z);
+
+        heart.position.sub(center);
+
+        scene.add(heart);
+
+        camera.position.set(0,0,maxDimension * 2);
+
+        controls.target.set(0,0,0);
+
+        controls.update();
+
+
+
+
         // Inspect materials
         heart.traverse((child) => {
 
             if (child.isMesh) {
+
+                child.castShadow = true;
+                child.receiveShadow = true;
 
                 console.log(child.material);
 
@@ -140,3 +192,5 @@ window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 });
+
+
